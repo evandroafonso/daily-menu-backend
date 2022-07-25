3a685cc9caa26159aa936aa0691aa83a21807181
evandroafonso <evandroafonsorodrigues@gmail.com>
2022-07-25 13:49:19 -0300

    add ProductServiceTest and delete entity menu and BaseDao

--- src/main/java/com/daily/menu/domain/model/baseDao/BaseDao.java
+++
@@ -1,9 +0,0 @@
-package com.daily.menu.domain.model.baseDao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.daily.menu.entity.baseEntity.BaseEntity;
-
-public interface BaseDao <T extends BaseEntity> extends JpaRepository<T, Long> {
-
-}
